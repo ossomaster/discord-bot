a57620ab341d3528f360922ef714f21eb052a3a0
osso <fdantealvarezt@gmail.com>
2023-02-07 00:31:36 -0500

    Agregar comandos: ardilla, risas, prrr

---
+++ commands/prrr.js
@@ -0,0 +1,55 @@
+const path = require("node:path");
+const {
+	joinVoiceChannel,
+	createAudioPlayer,
+	AudioPlayerStatus,
+	createAudioResource,
+	StreamType,
+} = require("@discordjs/voice");
+const { SlashCommandBuilder } = require("discord.js");
+
+const getUserVoiceConnection = (interaction) => {
+	const voiceChannelId = interaction.member.voice.channelId;
+	const voiceChannel = interaction.client.channels.cache.get(voiceChannelId);
+
+	return joinVoiceChannel({
+		channelId: voiceChannelId,
+		guildId: interaction.guild.id,
+		adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+	});
+};
+
+const playSong = (voiceConnection) => {
+	const player = createAudioPlayer();
+	const fileName = `../assets/sounds/prrr.mp3`;
+	const resource = createAudioResource(path.join(__dirname, fileName), {
+		inputType: StreamType.OggOpus,
+	});
+
+	player.play(resource);
+
+	const subscription = voiceConnection.subscribe(player);
+
+	player.on(AudioPlayerStatus.Idle, () => {
+		if (subscription) {
+			subscription.unsubscribe();
+		}
+	});
+
+	player.on("error", (error) => {
+		console.error(`Error: ${error.message} with resource`);
+	});
+};
+
+module.exports = {
+	data: new SlashCommandBuilder().setName("prrr").setDescription("Para cuando la kgues"),
+	async execute(interaction) {
+		try {
+			const voiceConnection = getUserVoiceConnection(interaction);
+			playSong(voiceConnection);
+			interaction.reply({ content: "** **", ephemeral: true });
+		} catch (error) {
+			console.error(error);
+		}
+	},
+};
